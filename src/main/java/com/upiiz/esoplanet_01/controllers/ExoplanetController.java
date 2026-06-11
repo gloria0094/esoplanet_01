e90a4c4894125ff13480b5d21586b220dae35f06
gloria0094 <144404571+gloria0094@users.noreply.github.com>
2026-06-11 13:22:01 -0600

    Fix API URL and enhance error handling in ExoplanetController
    Updated the FAST_API_URL variable and improved error logging.

--- src/main/java/com/upiiz/esoplanet_01/controllers/ExoplanetController.java
+++ src/main/java/com/upiiz/esoplanet_01/controllers/ExoplanetController.java
@@ -12,14 +12,16 @@ public class ExoplanetController {
 
     private final RestTemplate restTemplate = new RestTemplate();
     //private final String FAST_API_URL = "https://gloriaitzel0609-api-ia-python.hf.space/predict";
-    private final String String url_ia = "https://gloriaitzel0609-api-ia-python.hf.space/predict";
+    private final String FAST_API_URL  = "https://gloriaitzel0609-api-ia-python.hf.space/predict";
 
     @PostMapping("/classify")
-    public ResponseEntity<?> classifyExoplanet(@RequestBody Map<String, Object> datos, Object requestData) {
+    public ResponseEntity<?> classifyExoplanet(@RequestBody Map<String, Object> datos) {
         try {
-            ResponseEntity<Object> response = restTemplate.postForEntity(FAST_API_URL, requestData, Object.class);
+            ResponseEntity<Object> response = restTemplate.postForEntity(FAST_API_URL, datos, Object.class);
             return ResponseEntity.ok(response.getBody());
         } catch (Exception e) {
+            // Esto imprimirá el error real en los logs de Railway por si algo más se rompe
+            System.out.println("Error detallado en la llamada a la IA: " + e.getMessage());
             String jsonError = "{\"error\": \"Error al comunicarse con el modelo de IA\"}";
             return ResponseEntity.status(500).body(jsonError);
         }
